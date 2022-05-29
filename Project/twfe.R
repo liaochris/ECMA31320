cc4d525530e1e27dcf9009585df50d991473410d
Noah Sobel-Lewin <nvsl@uchicago.edu>
2022-05-29 18:32:06 -0500

    revise dtwfe figures

--- Project/twfe.R
+++ Project/twfe.R
@@ -15,248 +15,160 @@ if (Sys.info()[8] == "noahsobel-lewin") setwd("/Users/noahsobel-lewin/Dropbox/EC
 ################################################################################
 ################################ Dynamic TWFE  #################################
 ################################################################################
+# DTWFE function
+plot_dtwfe <- function(reg_data, controls) {
+
+  # assert control binary
+  stopifnot(controls %in% c(0,1))
+ 
+  # create formula quickly
+  if(controls == 0){
+    formula_felm <- as.formula(`HOUSEWORK HOURS` ~ 
+                                 `treat_dummy -5` + 
+                                 `treat_dummy -4` + 
+                                 `treat_dummy -3` + 
+                                 `treat_dummy -2` + 
+                                 `treat_dummy 0` + 
+                                 `treat_dummy 1` + 
+                                 `treat_dummy 2` + 
+                                 `treat_dummy 3` + 
+                                 `treat_dummy 4` + 
+                                 `treat_dummy 5` + 
+                                 `treat_dummy 6` + 
+                                 `treat_dummy 7` + 
+                                 `treat_dummy 8` + 
+                                 `treat_dummy 9` +
+                                 `treat_dummy 10` | f_id + f_year | 0 | STATE + id)
+    dyn_est <- felm(formula = formula_felm,
+                    data = reg_data, subset = between(Year, 1969, 1989))
+  }
+  if(controls == 1){
+    formula_felm <- as.formula(`HOUSEWORK HOURS` ~ 
+                                 `treat_dummy -5` + 
+                                 `treat_dummy -4` + 
+                                 `treat_dummy -3` + 
+                                 `treat_dummy -2` + 
+                                 `treat_dummy 0` + 
+                                 `treat_dummy 1` + 
+                                 `treat_dummy 2` + 
+                                 `treat_dummy 3` + 
+                                 `treat_dummy 4` + 
+                                 `treat_dummy 5` + 
+                                 `treat_dummy 6` + 
+                                 `treat_dummy 7` + 
+                                 `treat_dummy 8` + 
+                                 `treat_dummy 9` +
+                                 `treat_dummy 10` +
+                                 ann_hrs_work_control + 
+                                 labor_inc_control + 
+                                 age_control| f_id + f_year | 0 | STATE + id)
+    dyn_est <- felm(formula = formula_felm,
+                    data = reg_data, subset = between(Year, 1969, 1989))
+  }
+ 
+  dyn_est_ci <- lmtest::coefci(dyn_est, vcov = dyn_est$vcv)
+  
+  coef_years <- matrix(NA, nrow = length(start:end), ncol = 4)
+  coef_years[,1] <- start:end
+  for(i in start:end){
+    if(i == -1) coef_years[i - start + 1, 2:4] <- 0 
+    else{
+      coef_years[i - start + 1, 2] <- dyn_est$coefficients[grepl(paste0("`treat_dummy ", i, "`TRUE"), rownames(dyn_est$coefficients))]
+      coef_years[i - start + 1, 3:4] <- dyn_est_ci[grepl(paste0("`treat_dummy ", i, "`TRUE"), rownames(dyn_est$coefficients)),1:2]
+    }
+  }
+  coef_years <- coef_years[1:(dim(coef_years)[1] - 1),] %>% data.frame()
+  names(coef_years) <- c("year", "coef", "ci_low", "ci_high")
+  
+  plot <- ggplot(coef_years, aes(x=year, y = coef)) + 
+    geom_point(col = 'orange', shape = 17, size = 3) +
+    geom_line(col='orange') + 
+    geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.15, fill='orange') +
+    geom_hline(yintercept = 0, col = 'grey') + 
+    geom_vline(xintercept = 0, linetype= 'dashed', col = 'grey') + 
+    xlab("Years since ") + 
+    ylab("")+
+    theme(panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(), 
+          axis.line = element_line(colour = "black"),
+          text=element_text(size=16, family = 'Georgia'))
+  
+  return(plot)
+}
+
+# regressions
 
 # Read data 
-res_clean_wife <- fread("Data/final_regression_dataset.csv")
+res_clean_wife <- fread("Data/final_regression_dataset.csv") %>% 
+  select(c(
+    "id",
+    "AGE",
+    "ANNUAL HOURS WORKED",
+    "LABOR INCOME",
+    "Year",
+    "Unilateral Divorce Year",
+    "Equitable Distribution Year",
+    "Equitable Distribution",
+    "HOUSEWORK HOURS",
+    "STATE"))
 
 # dynamic:
 # make unit year fixed effects
 res_clean_wife[, `:=` (f_id = factor(id),
                        f_year = factor(Year))]
 
-#========== Unilateral divorce, no controls, ==========
+#========== Unilateral divorce ==========
+# define treatment
+res_clean_wife[, `Treatment Year` := `Unilateral Divorce Year`]
+               
+# make controls the value at year before treatment 
+res_clean_wife[, year_before_treat := (Year == `Treatment Year` -1)]
+res_clean_wife[, `:=`(age_control = max(year_before_treat * AGE),
+                       ann_hrs_work_control = max(year_before_treat * `ANNUAL HOURS WORKED`),
+                       labor_inc_control = max(year_before_treat * `LABOR INCOME`)
+                       ), by = id]
+
 # make treatment indicators
 start <- -5
 end <- 10 
-range <- end - start
-for(i in setdiff(0:range, c(4))){
-  res_clean_wife[[paste0("treat_dummy_", i)]] <- res_clean_wife[["Year"]] - res_clean_wife[["Unilateral Divorce Year"]] == (i-5)
+for(i in setdiff(end:start, -1)){
+  res_clean_wife[[paste0("treat_dummy ", i)]] <- res_clean_wife[["Year"]] - res_clean_wife[["Treatment Year"]] == (i-5)
 }
 
-# create formula quickly
-formula_felm <- as.formula(`HOUSEWORK HOURS` ~ 
-                             treat_dummy_0 + 
-                             treat_dummy_1 + 
-                             treat_dummy_2 + 
-                             treat_dummy_3 + 
-                             treat_dummy_5 + 
-                             treat_dummy_6 + 
-                             treat_dummy_7 + 
-                             treat_dummy_8 + 
-                             treat_dummy_9 + 
-                             treat_dummy_10 + 
-                             treat_dummy_11 + 
-                             treat_dummy_12 + 
-                             treat_dummy_13 + 
-                             treat_dummy_14 + 
-                             treat_dummy_15 | f_id + f_year | 0 | STATE + id)
-dyn_est <- felm(formula = formula_felm,
-          data = res_clean_wife, subset = between(Year, 1969, 1989))
 
-dyn_est_ci <- lmtest::coefci(dyn_est, vcov = dyn_est$vcv)
 
-coef_years <- matrix(NA, nrow = length(start:end), ncol = 4)
-coef_years[,1] <- start:end
-for(i in start:end){
-  if(i %in% c(-1)) coef_years[i + 5,2:4] <- 0 
-  else{
-    coef_years[i+5,2] <- dyn_est$coefficients[grepl(paste0("treat_dummy_", i +5, "TRUE"), rownames(dyn_est$coefficients))]
-    coef_years[i+5,3:4] <- dyn_est_ci[grepl(paste0("treat_dummy_", i +5, "TRUE"), rownames(dyn_est$coefficients)),1:2]
-  }
-}
-coef_years <- coef_years[1:(dim(coef_years)[1] - 1),] %>% data.frame()
-names(coef_years) <- c("year", "coef", "ci_low", "ci_high")
-
-ggplot(coef_years, aes(x=year, y = coef)) + 
-  geom_point(col = 'orange', shape = 17, size = 3) +
-  geom_line(col='orange') + 
-  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.15, fill='orange') +
-  geom_hline(yintercept = 0, col = 'grey') + 
-  geom_vline(xintercept = 0, linetype= 'dashed', col = 'grey') + 
-  xlab("Years since ") + 
-  ylab("")+
-  theme(panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(), 
-        axis.line = element_line(colour = "black"),
-        text=element_text(size=16, family = 'Georgia'))
-
+# no controls 
+plot_dtwfe(res_clean_wife, 0)
 ggsave("Graphics/dynamic_twfe_uni.png")
 
-#========== Unilateral divorce, controls ==========
-# create formula quickly
-formula_felm <- as.formula(`HOUSEWORK HOURS` ~ 
-                             treat_dummy_0 + 
-                             treat_dummy_1 + 
-                             treat_dummy_2 + 
-                             treat_dummy_3 + 
-                             treat_dummy_5 + 
-                             treat_dummy_6 + 
-                             treat_dummy_7 + 
-                             treat_dummy_8 + 
-                             treat_dummy_9 + 
-                             treat_dummy_10 + 
-                             treat_dummy_11 + 
-                             treat_dummy_12 + 
-                             treat_dummy_13 + 
-                             treat_dummy_14 + 
-                             treat_dummy_15 +
-                             `ANNUAL HOURS WORKED` + 
-                             `LABOR INCOME` + 
-                             AGE| f_id + f_year | 0 | STATE + id)
-dyn_est <- felm(formula = formula_felm,
-                data = res_clean_wife, subset = between(Year, 1969, 1989))
-
-dyn_est_ci <- lmtest::coefci(dyn_est, vcov = dyn_est$vcv)
-
-coef_years <- matrix(NA, nrow = length(start:end), ncol = 4)
-coef_years[,1] <- start:end
-for(i in start:end){
-  if(i %in% c(-1)) coef_years[i + 5,2:4] <- 0 
-  else{
-    coef_years[i+5,2] <- dyn_est$coefficients[grepl(paste0("treat_dummy_", i +5, "TRUE"), rownames(dyn_est$coefficients))]
-    coef_years[i+5,3:4] <- dyn_est_ci[grepl(paste0("treat_dummy_", i +5, "TRUE"), rownames(dyn_est$coefficients)),1:2]
-  }
-}
-coef_years <- coef_years[1:(dim(coef_years)[1] - 1),] %>% data.frame()
-names(coef_years) <- c("year", "coef", "ci_low", "ci_high")
-
-ggplot(coef_years, aes(x=year, y = coef)) + 
-  geom_point(col = 'orange', shape = 17, size = 3) +
-  geom_line(col='orange') + 
-  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.15, fill='orange') +
-  geom_hline(yintercept = 0, col = 'grey') + 
-  geom_vline(xintercept = 0, linetype= 'dashed', col = 'grey') + 
-  xlab("Years since ") + 
-  ylab("")+
-  theme(panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(), 
-        axis.line = element_line(colour = "black"),
-        text=element_text(size=16, family = 'Georgia'))
-
+# controls 
+plot_dtwfe(res_clean_wife, 1)
 ggsave("Graphics/dynamic_twfe_uni_controls.png")
 
-
-#========== Unilateral divorce + equitable distribution, no controls ==========
-# create treatment year var
+#========== Unilateral divorce + equitable distribution ==========
+# define treatment 
 res_clean_wife[, `Treatment Year` := max(`Equitable Distribution Year`,
                                            `Unilateral Divorce Year`,
                                            na.rm = TRUE), 
                  by = id]
-res_clean_wife[`Unilateral Divorce Year` == 0, `Treatment Year` := 0]
+res_clean_wife <- res_clean_wife[`Unilateral Divorce Year` == 0, 
+               `Treatment Year` := 0]
 
 # make treatment indicators
 start <- -5
 end <- 10 
-range <- end - start
-for(i in setdiff(0:range, c(4))){
-  res_clean_wife[[paste0("treat_dummy_", i)]] <- res_clean_wife[["Year"]] - 
-    res_clean_wife[["Treatment Year"]] == 
-    (i-5)
+for(i in setdiff(end:start, -1)){
+  res_clean_wife[[paste0("treat_dummy ", i)]] <- res_clean_wife[["Year"]] - res_clean_wife[["Treatment Year"]] == (i-5)
 }
 
-# create formula quickly
-formula_felm <- as.formula(`HOUSEWORK HOURS` ~ 
-                             treat_dummy_0 + 
-                             treat_dummy_1 + 
-                             treat_dummy_2 + 
-                             treat_dummy_3 + 
-                             treat_dummy_5 + 
-                             treat_dummy_6 + 
-                             treat_dummy_7 + 
-                             treat_dummy_8 + 
-                             treat_dummy_9 + 
-                             treat_dummy_10 + 
-                             treat_dummy_11 + 
-                             treat_dummy_12 + 
-                             treat_dummy_13 + 
-                             treat_dummy_14 + 
-                             treat_dummy_15 | f_id + f_year | 0 | STATE + id)
-dyn_est <- felm(formula = formula_felm,
-                data = res_clean_wife, subset = between(Year, 1969, 1989))
-
-dyn_est_ci <- lmtest::coefci(dyn_est, vcov = dyn_est$vcv)
-
-coef_years <- matrix(NA, nrow = length(start:end), ncol = 4)
-coef_years[,1] <- start:end
-for(i in start:end){
-  if(i %in% c(-1)) coef_years[i + 5,2:4] <- 0 
-  else{
-    coef_years[i+5,2] <- dyn_est$coefficients[grepl(paste0("treat_dummy_", i +5, "TRUE"), rownames(dyn_est$coefficients))]
-    coef_years[i+5,3:4] <- dyn_est_ci[grepl(paste0("treat_dummy_", i +5, "TRUE"), rownames(dyn_est$coefficients)),1:2]
-  }
-}
-coef_years <- coef_years[1:(dim(coef_years)[1] - 1),] %>% data.frame()
-names(coef_years) <- c("year", "coef", "ci_low", "ci_high")
-
-ggplot(coef_years, aes(x=year, y = coef)) + 
-  geom_point(col = 'orange', shape = 17, size = 3) +
-  geom_line(col='orange') + 
-  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.15, fill='orange') +
-  geom_hline(yintercept = 0, col = 'grey') + 
-  geom_vline(xintercept = 0, linetype= 'dashed', col = 'grey') + 
-  xlab("Years since ") + 
-  ylab("")+
-  theme(panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(), 
-        axis.line = element_line(colour = "black"),
-        text=element_text(size=16, family = 'Georgia'))
-
+# no controls 
+plot_dtwfe(res_clean_wife, 0)
 ggsave("Graphics/dynamic_twfe_equ.png")
 
-#========== Unilateral divorce + equitable distribution, controls ==========
-# create formula quickly
-formula_felm <- as.formula(`HOUSEWORK HOURS` ~ 
-                             treat_dummy_0 + 
-                             treat_dummy_1 + 
-                             treat_dummy_2 + 
-                             treat_dummy_3 + 
-                             treat_dummy_5 + 
-                             treat_dummy_6 + 
-                             treat_dummy_7 + 
-                             treat_dummy_8 + 
-                             treat_dummy_9 + 
-                             treat_dummy_10 + 
-                             treat_dummy_11 + 
-                             treat_dummy_12 + 
-                             treat_dummy_13 + 
-                             treat_dummy_14 + 
-                             treat_dummy_15 +
-                             `ANNUAL HOURS WORKED` + 
-                             `LABOR INCOME` + 
-                             AGE| f_id + f_year | 0 | STATE + id)
-dyn_est <- felm(formula = formula_felm,
-                data = res_clean_wife, subset = between(Year, 1969, 1989))
-
-dyn_est_ci <- lmtest::coefci(dyn_est, vcov = dyn_est$vcv)
-
-coef_years <- matrix(NA, nrow = length(start:end), ncol = 4)
-coef_years[,1] <- start:end
-for(i in start:end){
-  if(i %in% c(-1)) coef_years[i + 5,2:4] <- 0 
-  else{
-    coef_years[i+5,2] <- dyn_est$coefficients[grepl(paste0("treat_dummy_", i +5, "TRUE"), rownames(dyn_est$coefficients))]
-    coef_years[i+5,3:4] <- dyn_est_ci[grepl(paste0("treat_dummy_", i +5, "TRUE"), rownames(dyn_est$coefficients)),1:2]
-  }
-}
-coef_years <- coef_years[1:(dim(coef_years)[1] - 1),] %>% data.frame()
-names(coef_years) <- c("year", "coef", "ci_low", "ci_high")
-
-ggplot(coef_years, aes(x=year, y = coef)) + 
-  geom_point(col = 'orange', shape = 17, size = 3) +
-  geom_line(col='orange') + 
-  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.15, fill='orange') +
-  geom_hline(yintercept = 0, col = 'grey') + 
-  geom_vline(xintercept = 0, linetype= 'dashed', col = 'grey') + 
-  xlab("Years since ") + 
-  ylab("")+
-  theme(panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(), 
-        axis.line = element_line(colour = "black"),
-        text=element_text(size=16, family = 'Georgia'))
-
+# controls 
+plot_dtwfe(res_clean_wife, 1)
 ggsave("Graphics/dynamic_twfe_equ_controls.png")
+
+
 
